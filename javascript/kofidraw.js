81dc8dc84dba6fe8039a55b8d39bf378e2db6f5e
Zonoik <jonaneiro09@gmail.com>
2024-11-04 20:49:15 +0100

    javascript

---
+++ javascript/kofidraw.js
@@ -0,0 +1,6 @@
+kofiWidgetOverlay.draw('zonoik', {
+    'type': 'floating-chat',
+    'floating-chat.donateButton.text': 'Donate',
+    'floating-chat.donateButton.background-color': '#794bc4',
+    'floating-chat.donateButton.text-color': '#fff'
+  });
